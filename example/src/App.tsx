34c11533a0edc347de3b1b008da88e431b5e49b6
Leandro Gonçalves <lezinhomococa@hotmail.com>
2021-09-26 17:02:08 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-subtitles'
+import 'react-subtitles/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
